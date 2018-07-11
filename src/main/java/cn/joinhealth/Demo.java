7e863d5ab0b27a3f6708ccf910dc37eebdb857a4
xjl <xujinli0916@163.com>
2018-07-12 00:35:44 +0800

    idea项目第一次上传测试

---
+++ src/main/java/cn/joinhealth/Demo.java
@@ -0,0 +1,11 @@
+package cn.joinhealth;
+
+/**
+ * Created by xujin on 2018/7/12.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hello github！");
+    }
+}
