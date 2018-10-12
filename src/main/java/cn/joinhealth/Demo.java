30ce8aa33042602b42d6e18b50c2c7734d89c36d
jlxu <xujinli0916@163.com>
2018-10-12 09:08:25 +0800

    饱汉式单例测试

--- src/main/java/cn/joinhealth/Demo.java
+++ src/main/java/cn/joinhealth/Demo.java
@@ -7,7 +7,18 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("hello github push！");
-         System.out.println("hello github pull！");
+
+        System.out.println("hello github pull！");
+        System.out.println("hello github pull！2");
+
 
     }
+    /*public  static Demo singleTon=new Demo();
+    public static Demo getSingleTon() {
+        if (singleTon==null)
+            return new Demo();
+
+        return singleTon;
+
+    }*/
 }
