bd86703a9cf7940e78e164a755f8b48839385672
xjl <xujinli0916@163.com>
2018-10-12 08:54:54 +0800

    饱汉式单例测试

---
+++ src/main/java/cn/joinhealth/TestSingleton.java
@@ -0,0 +1,10 @@
+package cn.joinhealth;
+
+/**
+ * Created by xujin on 2018/10/8.
+ */
+public class TestSingleton {
+    public static void main(String[] args) {
+        /*System.out.println(Demo.getSingleTon());*/
+    }
+}
